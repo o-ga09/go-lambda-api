810e3238dfe62f2d23b752aa541dc0d450f67d99
taiti09 <taitiabe1997@gmail.com>
2024-03-01 06:28:46 +0900

    レスポンス修正

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -12,8 +13,30 @@ func main() {
 }
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	response := Response{
+		Count:  100,
+		Limit:  1,
+		Offset: 10,
+		User: User{
+			Name: "test",
+			Age:  "29",
+		},
+	}
+	json, _ := json.Marshal(response)
 	return events.APIGatewayProxyResponse{
-		Body:       "status ok !",
+		Body:       string(json),
 		StatusCode: 200,
 	}, nil
+}
+
+type Response struct {
+	Count  int64 `json:"count,omitempty"`
+	Limit  int64 `json:"limit,omitempty"`
+	Offset int64 `json:"offset,omitempty"`
+	User   User  `json:"user,omitempty"`
+}
+
+type User struct {
+	Name string
+	Age  string
 }
